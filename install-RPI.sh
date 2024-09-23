c71ae401b6b1abccb51f314f696bc372aa5ff80b
dennisppaul <dennis.paul@hfk-bremen.de>
2024-09-23 18:50:31 +0200

    added script to setup umgebung

---
+++ install-RPI.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# TODO this is not testet on RPI
+
+echo "+++ setting up umgebung      +++"
+
+echo "+++ updating package manager +++"
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+echo "+++ installing packages      +++"
+
+sudo apt-get install git clang mesa-utils cmake pkg-config libsdl2-dev libftgl-dev libglew-dev ffmpeg libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libavdevice-dev librtmidi-dev -y
+
+echo
