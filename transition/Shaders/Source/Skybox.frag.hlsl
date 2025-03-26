2f605a78eab3aec785ba21e0056654578f801f0f
dennisppaul <dennis.paul@hfk-bremen.de>
2025-03-26 23:14:51 +0100

    cleaned up

--- transition/Shaders/Source/Skybox.frag.hlsl
+++
@@ -1,7 +0,0 @@
-TextureCube<float4> SkyboxTexture : register(t0, space2);
-SamplerState SkyboxSampler : register(s0, space2);
-
-float4 main(float3 TexCoord : TEXCOORD0) : SV_Target0
-{
-    return SkyboxTexture.Sample(SkyboxSampler, TexCoord);
-}
